644779d388bb5fb2fa5e47eabe97cd82d5eac591
Fitra Salam S. Nagalay <alamsaungnaga@gmail.com>
2023-08-23 19:01:03 +0700

    template2 fix bug aos

--- components/Content/Footer.js
+++ components/Content/Footer.js
@@ -12,7 +12,7 @@ export default function Footer({}) {
   }, []);
   return (
     <>
-      <div className={"bg-footer bg-cover bg-no-repeat bg-center "}>
+      <div className={"bg-footer bg-cover bg-no-repeat bg-center "} data-aos="fade-in" data-aos-duration="3000">
         <Image src={MaskBottom} className="w-full" />
         <div className={"container mx-auto pt-5 pb-20"}>
           <div className={"flex justify-center items-center content-center"} data-aos="flip-left" data-aos-duration="2000">
@@ -24,7 +24,7 @@ export default function Footer({}) {
               With Love,
             </p>
           </div>
-          <div className={"flex justify-center items-center"} data-aos="flip-left" data-aos-duration="2000">
+          <div className={"flex justify-center items-center"}>
             <h1
               className={
                 "md:text-6xl text-center  text-5xl mb-0 font-medium text-white leading-relaxed font-curs"
@@ -33,7 +33,7 @@ export default function Footer({}) {
               &nbsp; {data.nama_pria} &nbsp;
             </h1>
           </div>
-          <div className={"flex justify-center items-center content-center "} data-aos="fade-in" data-aos-duration="3000">
+          <div className={"flex justify-center items-center content-center "}>
             <h1
               className={
                 "md:text-6xl text-center text-5xl mb-0 font-medium text-white leading-relaxed font-curs"
@@ -42,7 +42,7 @@ export default function Footer({}) {
               &{" "}
             </h1>
           </div>
-          <div className={"flex justify-center items-center content-center "} data-aos="flip-right" data-aos-duration="2000">
+          <div className={"flex justify-center items-center content-center "}>
             <h1
               className={
                 "md:text-6xl text-center  text-5xl mb-5 font-medium text-white leading-relaxed font-curs"
